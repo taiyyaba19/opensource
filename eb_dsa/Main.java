612f9c4437825c911930128e93edbdb5965209aa
taiyyaba19 <mdtaiyyababegum786@gmail.com>
2024-11-12 10:34:30 +0530

    Create Main.java

---
+++ eb_dsa/Main.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class solution {
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+    double X = sc.nextInt();
+    double P = X / 0.95;
+    System.out.println("%.4f\n",P);
+  }
+}
