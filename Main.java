2179d69d2be9bd40e3da9604386af994c8d5ac30
taiyyaba19 <mdtaiyyababegum786@gmail.com>
2024-11-12 10:27:52 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+import java.io.*;
+import java.util.*;
+public class solution {
+  public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+      double X = sc.nextdouble();
+      
